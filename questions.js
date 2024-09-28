1755ca13386ef0e5f0214794c5b569f2fa110e76
Sandra C S <101389751+Sandrasebi29@users.noreply.github.com>
2024-09-28 22:59:22 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,128 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "High Tech Markup Language",
+      "Hyperlink Text Markup Language",
+      "Home Tool Markup Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which technology is primarily responsible for the styling of web pages?",
+    answer: "CSS",
+    options: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "PYTHON"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which programming language is mainly used for adding interactivity to websites?",
+    answer: "JavaScript",
+    options: [
+      "HTML",
+      "CSS",
+      "Python",
+      "JavaScript"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a back-end programming language commonly used in web development?",
+    answer: "HTML",
+    options: [
+      "PHP",
+      "Ruby",
+      "Java",
+      "HTML"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which type of web development allows for both front-end and back-end development using a single language?",
+    answer: "Full-stack development",
+    options: [
+      "Full-stack development",
+      "Cross-platform development",
+      "Multi-Language development",
+      "Hybrid development"
+    ]
+  },
+  
+
+    {
+     numb: 6,
+     question: "What is the purpose of the script tag in HTML?",
+     answer: "To include external JavaScript code",
+     options: [
+      "To define the page’s structure",
+      "To include external CSS styles",
+      "To include external JavaScript code",
+       "To create hyperlinks"
+     ]
+   },
+
+
+   {
+    numb: 7,
+    question: "What is the correct syntax for creating a CSS class called “highlight” with a red text color?",
+    answer: ".highlight { color: red; }",
+    options: [
+     ".highlight { color: red; }",
+     "highlight { text-color: red; }",
+     ".highlight { text-color: red; }",
+      " highlight { color: red; }"
+    ]
+  },
+
+
+  {
+    numb: 8,
+    question: " Which HTML tag is used to create a hyperlink?",
+    answer: "{a}",
+    options: [
+     "{a}",
+     "{link}",
+     "{h1}",
+      " {p}"
+    ]
+  },
+
+
+  {
+    numb: 9,
+    question: " What is the purpose of the “alt” attribute in an  tag?",
+    answer: "It provides alternative text if the image fails to load",
+    options: [
+     "It specifies the alignment of the image",
+     "It provides alternative text if the image fails to load",
+     " It sets the size of the image",
+      " It defines the image source"
+    ]
+  },
+
+
+
+  {
+    numb: 10,
+    question: "Which of the following is a popular front-end development framework maintained by Google?",
+    answer: "Angular",
+    options: [
+     "React",
+     "Angular",
+     "Vue.js",
+      " Django"
+    ]
+  },
+
+
+
+   
+];
